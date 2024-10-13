9e6a87c46da81511a2d3ff1dd88ce4c9a3b0547c
suprohub <suprohub@gmail.com>
2024-10-13 12:00:01 +0400

    super commit

--- examples/minimal-winit/src/main.rs
+++ examples/minimal-winit/src/main.rs
@@ -1,14 +1,17 @@
 #![deny(clippy::all)]
 #![forbid(unsafe_code)]
 
+use std::sync::Arc;
+
 use error_iter::ErrorIter as _;
 use log::error;
 use pixels::{Error, Pixels, SurfaceTexture};
+use winit::application::ApplicationHandler;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::EventLoop;
 use winit::keyboard::KeyCode;
-use winit::window::WindowBuilder;
+use winit::window::Window;
 use winit_input_helper::WinitInputHelper;
 
 const WIDTH: u32 = 320;
@@ -23,65 +26,80 @@ struct World {
     velocity_y: i16,
 }
 
+#[derive(Default)]
+pub struct App<'win> {
+    window: Option<Arc<Window>>,
+    world: World,
+    pixels: Option<Pixels<'win>>
+}
+
+impl<'win> ApplicationHandler for App<'win> {
+    fn window_event(
+            &mut self,
+            event_loop: &winit::event_loop::ActiveEventLoop,
+            window_id: winit::window::WindowId,
+            event: WindowEvent,
+        ) {
+        match event {
+            WindowEvent::RedrawRequested => {
+                if let Some(pixels) = self.pixels.as_mut() {
+                    self.world.draw(pixels.frame_mut());
+                    if let Err(err) = pixels.render() {
+                        log_error("pixels.render", err);
+                        event_loop.exit();
+                        return;
+                    }
+                }
+            },
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            },
+            WindowEvent::Resized(size) => {
+                if let Some(pixels) = self.pixels.as_mut() {
+                    if let Err(err) = pixels.resize_surface(size.width, size.height) {
+                        log_error("pixels.resize_surface", err);
+                        event_loop.exit();
+                        return;
+                    }
+                }
+            },
+            _ => ()
+        }
+    }
+
+    fn device_event(
+            &mut self,
+            event_loop: &winit::event_loop::ActiveEventLoop,
+            device_id: winit::event::DeviceId,
+            event: winit::event::DeviceEvent,
+        ) {
+        
+    }
+
+    fn about_to_wait(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        
+    }
+
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        if let Ok(window) = event_loop.create_window(Window::default_attributes()) {
+            let window = Arc::new(window);
+            self.window = Some(window.clone());
+            let window_size = window.inner_size();
+            let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, window.clone());
+            if let Ok(pixels) = Pixels::new(WIDTH, HEIGHT, surface_texture) {
+                self.pixels = Some(pixels)
+            }
+        }
+    }
+
+}
+
 fn main() -> Result<(), Error> {
     env_logger::init();
     let event_loop = EventLoop::new().unwrap();
-    let mut input = WinitInputHelper::new();
-    let window = {
-        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
-        WindowBuilder::new()
-            .with_title("Hello Pixels")
-            .with_inner_size(size)
-            .with_min_inner_size(size)
-            .build(&event_loop)
-            .unwrap()
-    };
-
-    let mut pixels = {
-        let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH, HEIGHT, surface_texture)?
-    };
-    let mut world = World::new();
-
-    let res = event_loop.run(|event, elwt| {
-        // Draw the current frame
-        if let Event::WindowEvent {
-            event: WindowEvent::RedrawRequested,
-            ..
-        } = event
-        {
-            world.draw(pixels.frame_mut());
-            if let Err(err) = pixels.render() {
-                log_error("pixels.render", err);
-                elwt.exit();
-                return;
-            }
-        }
-
-        // Handle input events
-        if input.update(&event) {
-            // Close events
-            if input.key_pressed(KeyCode::Escape) || input.close_requested() {
-                elwt.exit();
-                return;
-            }
-
-            // Resize the window
-            if let Some(size) = input.window_resized() {
-                if let Err(err) = pixels.resize_surface(size.width, size.height) {
-                    log_error("pixels.resize_surface", err);
-                    elwt.exit();
-                    return;
-                }
-            }
-
-            // Update internal state and request a redraw
-            world.update();
-            window.request_redraw();
-        }
-    });
-    res.map_err(|e| Error::UserDefined(Box::new(e)))
+    let mut app = App::default();
+    event_loop.run_app(&mut app).unwrap();
+    Ok(())
 }
 
 fn log_error<E: std::error::Error + 'static>(method_name: &str, err: E) {
@@ -135,6 +153,18 @@ impl World {
             };
 
             pixel.copy_from_slice(&rgba);
+        }
+    }
+}
+
+
+impl Default for World {
+    fn default() -> Self {
+        Self {
+            box_x: 24,
+            box_y: 16,
+            velocity_x: 1,
+            velocity_y: 1,
         }
     }
 }
